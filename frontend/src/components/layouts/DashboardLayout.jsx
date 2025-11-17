1839b5a007cc31bd2bd77704d882a2f437f76bbe
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-11-17 09:13:38 -0600

    Changes

--- frontend/src/components/layouts/DashboardLayout.jsx
+++ frontend/src/components/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserContext } from "../../context/userContext";
+import { UserContext } from "../../context";
 import Navbar from "./Navbar";
 import SideMenu from "./SideMenu";
 
